cfa380f758356d2328ca95453566c3eb5f8aedda
gokhangemici <gokhan_gemici@hotmail.com>
2021-08-27 20:25:23 +0300

    added homework2

--- post.php
+++ post.php
@@ -23,3 +23,38 @@
  * - `getPostDetails` fonksiyonu tetiklenerek ilgili içeriğin çıktısı gösterilmeli.
  */
 
+include_once "functions.php"; //we included our source file to leverage its functionalities.
+/*
+    if id variable is not exist, then create it and set id to 1, 
+    if it is set, leave its value as it is. Do same tasks for title and type variables too. 
+    I can set any value that I want to the type and title variables.
+*/
+if(!isset($id)){
+     $id = 1; 
+}
+else{
+     $id = $key; 
+}
+if(!isset($title)){ 
+     $title = "BTK Akademi ile Veri Bilimi Derslerine Hoşgeldiniz"; 
+} 
+else{ 
+    $title = $value['title']; 
+}
+if(!isset($type)){ 
+    $type = "urgent"; 
+} 
+else{ 
+    $type = $value['type']; 
+}
+// I created an array which holds color information according to the value of type variable (urgent => red, warning => yellow, normal => none, blue => blue)
+$bgColor = array("urgent" => "#B71C1C", "warning" => "#FFEB3B", "normal" => "#FFFFFF", "blue" => "#0277BD");
+
+echo "<div style = 'background-color:". $bgColor[$type]."; '<br>"; //background color was set and opened a div tag with style property. 
+echo getPostDetails($id,$title); //triggered getPostDetails to run. 
+// It shows us the content of main post located in getPostDetails(). 
+echo "</div>"; // Closed div tag. 
+?>
+
+
+
